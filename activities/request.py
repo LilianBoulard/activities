57c5737f3999a3fde242c42debc5bfdffdbeada2
Paul-JD <81415058+Paul-JD@users.noreply.github.com>
2022-05-20 16:42:29 +0200

    push

--- activities/request.py
+++ activities/request.py
@@ -1,7 +1,37 @@
 """
 Implements a request between the output of NLTK and the Redis database.
+
+4 attributs:
+    -Name
+    -Tag
+    -Price
+    -localisation
 """
 
 
 class Request:
-    pass
+
+    def __init__(self) -> None:
+        pass
+
+    """
+    DB connection
+    """
+    
+    """
+    Name Function
+    """
+
+    """
+    Date function
+    """
+
+    """
+    Localisation function
+    """
+
+    """
+    Tag function
+    """
+    
+    
