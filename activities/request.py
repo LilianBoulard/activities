c4a9dfb547a6e5026576ccdf5043260da999e4c1
Paul-JD <81415058+Paul-JD@users.noreply.github.com>
2022-05-26 16:54:45 +0200

    Init request.py

--- activities/request.py
+++ activities/request.py
@@ -9,10 +9,21 @@ Implements a request between the output of NLTK and the Redis database.
 """
 
 
+from ast import Str
+
+
 class Request:
 
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self, title = None, emplacement = None, theme = None, date = None, price = None) -> None:
+        
+        self.title = title #Str
+        self.emplacement = emplacement #Str
+        self.theme = theme #Str
+        self.date = date #DateTime
+        self.price = price #Bool or Range
+        self.requete = None
+
 
     """
     DB connection
@@ -21,6 +32,14 @@ class Request:
     """
     Name Function
     """
+    def set_title_event(self, titre) -> None:       
+        self.title =titre
+    
+    def get_title_event(self) -> Str:
+        return self.title
+    
+
+    
 
     """
     Date function
