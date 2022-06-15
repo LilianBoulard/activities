ed73b54fceec2f1ddeaebe843ace2227f12f0d17
Lilian <lilian@boulard.fr>
2022-06-15 23:05:32 +0200

    Update pulling code

--- pull_data.py
+++ pull_data.py
@@ -7,12 +7,20 @@ import requests
 
 from functools import reduce
 from datetime import datetime
+from itertools import zip_longest
 from dateutil.parser import isoparse
-from typing import Dict, List, Any, Optional
+from pydantic import ValidationError
+from typing import List, Optional, Generator, Iterable
 
-from activities.database.redis import RedisDatabase
-from activities.database.redis.models import Event
+from activities.database.redis import db, Event
 from activities.utils import decode_json, price_regex
+
+
+def batcher(iterable: Iterable, n: int):
+    # Iterate a list in batches of size n.
+    # From https://stackoverflow.com/a/34166690/9084059
+    args = [iter(iterable)] * n
+    return zip_longest(*args)
 
 
 headers = {
@@ -26,6 +34,10 @@ headers = {
 
 
 def get_event_from_row(row: dict) -> Optional[Event]:
+
+    identifier = row.get('id', None)
+    if not identifier:
+        return
 
     title = row.get('title', None)
     if not title:
@@ -132,55 +144,57 @@ def get_event_from_row(row: dict) -> Optional[Event]:
     if not lat or not lon:
         return
 
-    return Event(
-        title=title,
-        tags=tags,
+    try:
+        event = Event(
+            identifier=identifier,
+            title=title,
+            tags=tags,
 
-        reservation_required=reservation_required,
-        reservation_url=reservation_url,
-        reservation_url_description=reservation_url_description,
+            reservation_required=reservation_required,
+            reservation_url=reservation_url,
+            reservation_url_description=reservation_url_description,
 
-        date_start=date_start,
-        date_end=date_end,
+            date_start=date_start,
+            date_end=date_end,
 
-        price_type=price_type,
-        price_detail=price_detail,
-        price_start=min(all_prices),
-        price_end=max(all_prices),
+            price_type=price_type,
+            price_detail=price_detail,
+            price_start=min(all_prices),
+            price_end=max(all_prices),
 
-        contact_url=row.get('contact_url', ''),
-        contact_mail=row.get('contact_mail', ''),
-        contact_phone=row.get('contact_phone', ''),
-        contact_facebook=row.get('contact_facebook', ''),
-        contact_twitter=row.get('contact_twitter', ''),
+            contact_url=row.get('contact_url', ''),
+            contact_mail=row.get('contact_mail', ''),
+            contact_phone=row.get('contact_phone', ''),
+            contact_facebook=row.get('contact_facebook', ''),
+            contact_twitter=row.get('contact_twitter', ''),
 
-        place=place,
-        street=street,
-        city=city,
-        zipcode=zipcode,
-        department=department,
-        district=district,
+            place=place,
+            street=street,
+            city=city,
+            zipcode=zipcode,
+            department=department,
+            district=district,
 
-        latitude=lat,
-        longiture=lon,
+            latitude=lat,
+            longiture=lon,
 
-        blind=bool(row.get('blind', 0)),
-        deaf=bool(row.get('deaf', 0)),
-        pmr=bool(row.get('pmr', 0)),
-    )
+            blind=bool(row.get('blind', 0)),
+            deaf=bool(row.get('deaf', 0)),
+            pmr=bool(row.get('pmr', 0)),
+        )
+    except ValidationError:
+        print('Invalid event')
+    else:
+        return event
 
 
-def get_clean_events(records: list) -> List[Event]:
-    clean_events: List[Event] = []
-
+def get_clean_events(records: list) -> Generator[Event, None, None]:
     for record in records:
         index: int
 
         event = get_event_from_row(record['fields'])
         if event is not None:
-            clean_events.append(event)
-
-    return clean_events
+            yield event
 
 
 def que_faire_a_paris() -> None:
@@ -190,29 +204,12 @@ def que_faire_a_paris() -> None:
     If the database has already been downloaded today, it is read from disk.
     Source: https://opendata.paris.fr/explore/dataset/que-faire-a-paris-/
     """
-    if RedisDatabase.dbfilename.exists():
-        # If the database file was modified for the last time before the last
-        # time the data source was updated (on the website),
-        # reset the database and add the new data to it.
-        last_mod_time = RedisDatabase.dbfilename.lstat().st_mtime
-        last_mod = datetime.fromtimestamp(last_mod_time)
-        now = datetime.now()
-        last_datasource_update = datetime(now.year, now.month, now.day, hour=7)
-        if last_mod > last_datasource_update:
-            reset_db = True
-        else:
-            reset_db = False
-    else:
-        reset_db = False
 
-    rdb: RedisDatabase = RedisDatabase()
-    # If we realize that the db is empty, we'll add the new data regardless.
-    if not rdb.is_empty():
-        if reset_db:
-            rdb.reset()
-            RedisDatabase.dbfilename.unlink()
-        else:
-            return
+    # Get all the event ids we got in the database
+    db_record_ids = {}
+    for key_batch in batcher(db.scan_iter('user:*'), 500):
+        db_record_ids.update(key_batch)
+
     # Searches for the 10k first events, with the timezone set on Paris.
     endpoint = ("https://opendata.paris.fr/api/records/1.0/search/"
                 "?dataset=que-faire-a-paris-&q="
@@ -222,9 +219,11 @@ def que_faire_a_paris() -> None:
     with requests.get(endpoint, headers=headers) as r:
         data = decode_json(r.text)
 
-    records: List[Dict[str, Any]] = data['records']
-    rdb.bulk_add(get_clean_events(records))
-    rdb.save()
+    for record in data['records']:
+        if int(record['fields']['id']) in db_record_ids:
+            continue
+        event = get_event_from_row(record['fields'])
+        event.save()
 
 
 if __name__ == "__main__":
