fb3e5ba474b807665c4718198f7240a20ac76720
Lilian <lilian@boulard.fr>
2022-06-22 17:56:16 +0200

    Add a tolerance to the price parsing

--- activities/nlp/parsers/price.py
+++ activities/nlp/parsers/price.py
@@ -1,6 +1,6 @@
 import re
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 
 _euro = "€|eur|EUR|euros|euro|Euro|Euros|balles"
@@ -14,10 +14,33 @@ class PriceParser:
     Currently, uses a regex.
     """
 
+    # This variable is used when the min price is equal to the max price:
+    # in this case, if the value is in the range (1), then some respective
+    # offsets are applied to the prices.
+    # For example, if we got (min_price=4, max_price=4), and if `_tolerances`
+    # had an entry (range(1, 10), (1, 1)), then our final values would be
+    # (min_price=3, max_price=5).
+    # If the price is not in any range then the last one
+    # (assumed to be the highest) is used.
+    # See the `_get_tolerance` method for more info.
+    _tolerances: List[Tuple[range, Tuple[int, int]]] = [
+        (range(1, 10), (1, 1)),
+        (range(10, 20), (2, 2)),
+        (range(30, 50), (5, 5)),
+    ]
+
     def _clean_match(self, match: str) -> str:
         return match.replace(',', '.')
 
+    def _get_tolerance(self, price: int) -> Tuple[int, int]:
+        for rng, tolerance in self._tolerances:
+            if price in rng:
+                return tolerance
+        else:
+            return self._tolerances[-1][1]
+
     def __call__(self, sentence: str) -> Optional[Tuple[int, int]]:
+
         prices_found = re.findall(price_regex, sentence)
 
         # FIXME: Hackish
@@ -44,4 +67,11 @@ class PriceParser:
             # We did not find any price
             return
 
-        return min(all_prices), max(all_prices)
+        min_price, max_price = min(all_prices), max(all_prices)
+        # If the min/max prices are the same, add a tolerance to each
+        if min_price == max_price:
+            tolerance_left, tolerance_right = self._get_tolerance(min_price)
+            min_price -= tolerance_left
+            max_price -= tolerance_right
+
+        return min_price, max_price
