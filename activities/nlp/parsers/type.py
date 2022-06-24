88ca01f9ad205c45270a0e165b2897d30a8066bd
Lilian <lilian@boulard.fr>
2022-06-24 15:21:48 +0200

    Add type parser based on conceptnet

--- activities/nlp/parsers/type.py
+++ activities/nlp/parsers/type.py
@@ -4,17 +4,31 @@ Resources:
 - Semantic Association
 """
 
-from typing import Optional, List, Set
+import pickle
 
+from typing import Optional, Set
+from networkx import MultiDiGraph
+
+from ...config import project_root
 from ...database.redis import Event
 
 
 def get_unique_tags() -> Set[str]:
-    events = Event.all_pks()
+    pks = Event.all_pks()
     tags = set()
-    for event in events:
-        tags.update(event.tags.split(';'))
+    for pk in pks:
+        event = Event.get(pk)
+        tags.update(event.tags)
     return tags
+
+
+def load_conceptnet() -> MultiDiGraph:
+    file = project_root.parent / 'conceptnet.gpickle'
+    if not file.exists():
+        raise RuntimeError(
+            'Could not load ConceptNet graph, launch `pull_data.py`.'
+        )
+    return pickle.load(file.open(mode='rb'))
 
 
 class TypeParser:
@@ -25,8 +39,39 @@ class TypeParser:
 
     # At runtime, collect all the unique types found in the database.
     unique_types = get_unique_tags()
+    graph: MultiDiGraph = load_conceptnet()
 
-    def __call__(self, tokens: List[str]) -> Optional[List[str]]:
-        # Remove the duplicates in the tokens
-        # (we don't want to explore the graph twice for the same token)
-        tokens = set(tokens)
+    def __call__(self, tokens: Set[str]) -> Optional[Set[str]]:
+        tags = set()
+
+        for token in tokens:
+
+            if not self.graph.has_node(token):
+                # The token is not in the graph, skip
+                continue
+
+            related = set(self.graph.successors(token))
+            # Keep the intersection between the tags in the database and the
+            # successors of the token in the graph
+            related_tags = self.unique_types.intersection(related)
+
+            if len(related_tags) == 0:
+                continue
+            elif len(related_tags) == 1:
+                tags.update(related_tags)
+            elif len(related_tags) > 1:
+                # If there are multiple possibilities for a single token,
+                # return the one with the highest weight (if equal, return any)
+                weights = {
+                    tag: sum([
+                        data['weight']
+                        for (rel, data) in self.graph.get_edge_data(token, tag).items()
+                    ])
+                    for tag in related_tags
+                }
+                # To descending order
+                weights = dict(sorted(weights, key=lambda pair: pair[1], reverse=True))
+                # Keep the first, which is the one with the highest weight
+                tags.update(list(weights.items())[0][0])
+
+        return tags
